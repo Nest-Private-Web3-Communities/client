ab025da484d03a4590a317d3ac541e4d36695687
Spandan Barve <spandan567@gmail.com>
2024-02-12 21:19:43 +0530

    Theme functionality

--- src/contexts/globalContext.tsx
+++ src/contexts/globalContext.tsx
@@ -1,18 +1,53 @@
-import React, { ReactNode, createContext, useContext, useState } from "react";
+import React, {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { Theme } from "../hooks/useTheme";
 
 interface GlobalContextType {
   modalState: {
     modal: ReactNode;
     setModal: React.Dispatch<React.SetStateAction<React.ReactNode>>;
   };
+  themeState: {
+    theme: Theme;
+    setTheme: React.Dispatch<React.SetStateAction<Theme>>;
+  };
 }
 
 const GlobalContext = createContext<GlobalContextType>({} as GlobalContextType);
 
 export function GlobalContextProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useState<Theme>("default");
   const [modal, setModal] = useState<ReactNode | null>();
 
-  const value = { modalState: { modal, setModal } };
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("vitess__arewefastyet__theme");
+    if (storedTheme == "dark" || storedTheme == "default") {
+      setTheme(storedTheme);
+    } else {
+      const darkColorPreference = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      );
+
+      setTheme(darkColorPreference.matches ? "dark" : "default");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme) {
+      document.documentElement.setAttribute("data-theme", theme);
+      localStorage.setItem("vitess__arewefastyet__theme", theme);
+    }
+  }, [theme]);
+
+  const value = {
+    modalState: { modal, setModal },
+    themeState: { theme, setTheme },
+  };
 
   return (
     <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
